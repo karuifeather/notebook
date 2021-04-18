682e2d221282c22fc40cecf3c40378825d354da8
karuifeather <aryalaashaya@gmail.com>
2021-04-18 23:40:54 +0545

    test with esbuild-wasm@0.11.12: not working

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,46 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React, { useState, useEffect, useRef } from 'react';
+import ReactDOM from 'react-dom';
+import * as esbuild from 'esbuild-wasm';
 
 const App = () => {
-    return <div>Hi there!</div>;
-}
+  const ref = useRef<any>();
+  const [rawCode, setRawCode] = useState('');
+  const [code, setCode] = useState('');
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+  const startService = async () => {
+    ref.current = await esbuild.initialize({
+      //   worker: true,
+      wasmURL: '/esbuild.wasm',
+    });
+  };
+
+  useEffect(() => {
+    startService();
+  }, []);
+
+  const onClick = () => {
+    console.log(ref.current);
+
+    if (!ref.current) return;
+    console.log(ref.current);
+  };
+
+  return (
+    <>
+      <div>
+        <div className=''>
+          <textarea
+            value={rawCode}
+            onChange={(e) => setRawCode(e.target.value)}
+          ></textarea>
+        </div>
+        <div onClick={onClick} className=''>
+          <button>Submit</button>
+        </div>
+      </div>
+      <pre>{code}</pre>
+    </>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
