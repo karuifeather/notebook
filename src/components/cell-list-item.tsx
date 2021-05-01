949d2c920b85bd32b86dde28533fbcb3481c99d1
karuifeather <aryalaashaya@gmail.com>
2021-05-02 00:33:32 +0545

    added action-bar to move and delete cells

--- src/components/cell-list-item.tsx
+++ src/components/cell-list-item.tsx
@@ -1,6 +1,8 @@
+import './cell-list-item.css';
 import { Cell } from '../state';
 import CodeCell from './code-cell';
 import TextEditor from './text-editor';
+import ActionBar from './action-bar';
 
 interface CellListItemProps {
   cell: Cell;
@@ -9,10 +11,24 @@ interface CellListItemProps {
 const CellListItem: React.FC<CellListItemProps> = ({ cell }) => {
   let child: JSX.Element;
 
-  if (cell.type === 'code') child = <CodeCell cell={cell} />;
-  else child = <TextEditor cell={cell} />;
+  if (cell.type === 'code')
+    child = (
+      <>
+        <div className='action-bar-wrapper'>
+          <ActionBar id={cell.id} />
+        </div>
+        <CodeCell cell={cell} />
+      </>
+    );
+  else
+    child = (
+      <>
+        <ActionBar id={cell.id} />
+        <TextEditor cell={cell} />
+      </>
+    );
 
-  return <div>{child}</div>;
+  return <div className='cell-list-item'>{child}</div>;
 };
 
 export default CellListItem;
