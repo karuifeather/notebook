22109313ee8e15448eff68a2cdf297c9a2dbdd37
Aashaya Aryal <aryalaashaya@gmail.com>
2024-12-12 04:51:09 -0600

    added file exts in imports

--- src/bundler/index.ts
+++ src/bundler/index.ts
@@ -1,7 +1,7 @@
 import * as esbuild from 'esbuild-wasm';
 
-import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
-import { fetchPlugin } from './plugins/fetch-plugin';
+import { unpkgPathPlugin } from './plugins/unpkg-path-plugin.ts';
+import { fetchPlugin } from './plugins/fetch-plugin.ts';
 
 // Define the return object for better type safety
 interface BuildReturnObject {
