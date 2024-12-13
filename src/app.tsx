24c4b590b67d69988a0c0465ad26ea906842f6ba
Aashaya Aryal <aryalaashaya@gmail.com>
2024-12-13 14:27:41 -0600

    dark mode enabled

--- src/app.tsx
+++ src/app.tsx
@@ -6,8 +6,20 @@ import Home from '@/pages/home.tsx';
 import CellList from '@/components/cell-list/cell-list.tsx';
 
 import './style.scss';
+import { useDarkMode } from './hooks/use-dark-mode.ts';
+import { useEffect } from 'react';
 
 export const App = () => {
+  const isDarkMode = useDarkMode();
+
+  useEffect(() => {
+    if (isDarkMode) {
+      document.documentElement.classList.add('dark');
+    } else {
+      document.documentElement.classList.remove('dark');
+    }
+  }, [isDarkMode]);
+
   return (
     <Provider store={store}>
       <Router>
