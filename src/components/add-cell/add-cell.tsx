aeb44108dd6862485dcc6142e3b6d5d861d15c89
Aashaya Aryal <aryalaashaya@gmail.com>
2024-12-13 21:31:24 -0600

    customized add-cell

--- src/components/add-cell/add-cell.tsx
+++ src/components/add-cell/add-cell.tsx
@@ -1,6 +1,4 @@
 import React from 'react';
-
-import './add-cell.css';
 import { useActions } from '@/hooks/use-actions.ts';
 
 interface AddCellProps {
@@ -12,28 +10,34 @@ const AddCell: React.FC<AddCellProps> = ({ prevCellId, forceVisible }) => {
   const { insertCellAfter } = useActions();
 
   return (
-    <div className={`add-cell ${forceVisible && 'visible'}`}>
-      <div className="add-buttons">
+    <div
+      className={`flex items-center my-4 ${
+        forceVisible ? '' : 'opacity-50 hover:opacity-100 transition-opacity'
+      }`}
+    >
+      {/* Divider Line */}
+      <div className="flex-grow h-px bg-gray-300 dark:bg-gray-600" />
+
+      {/* Add Buttons */}
+      <div className="flex items-center justify-center mx-4 space-x-2">
         <button
-          className="button is-primary is-rounded is-small"
+          className="relative flex items-center px-4 py-2 bg-primary-light dark:bg-primary-dark text-white rounded shadow hover:bg-blue-600 dark:hover:bg-green-700 transition"
           onClick={() => insertCellAfter(prevCellId, 'code')}
         >
-          <span className="icon is-small">
-            <i className="fas fa-plus" />
-          </span>
-          <span>Code</span>
+          <span className="mr-2">+</span>
+          <i className="fas fa-code mr-2" />
         </button>
         <button
-          className="button is-primary is-rounded is-small"
+          className="flex items-center px-4 py-2 bg-primary-light dark:bg-primary-dark text-white rounded shadow hover:bg-blue-600 dark:hover:bg-green-700 transition"
           onClick={() => insertCellAfter(prevCellId, 'text')}
         >
-          <span className="icon is-small">
-            <i className="fas fa-plus" />
-          </span>
-          <span>Text</span>
+          <span className="mr-2">+</span>
+          <i className="fas fa-font mr-2" />
         </button>
       </div>
-      <div className="divider" />
+
+      {/* Divider Line */}
+      <div className="flex-grow h-px bg-gray-300 dark:bg-gray-600" />
     </div>
   );
 };
