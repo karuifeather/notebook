131134b285472ecde15c9847368c37cf3b8ef289
Aashaya Aryal <aryalaashaya@gmail.com>
2024-12-18 10:06:49 -0600

    action-bar: update styling

--- src/components/cells/action-bar.tsx
+++ src/components/cells/action-bar.tsx
@@ -11,14 +11,16 @@ const ActionBar: React.FC<ActionBarProps> = ({ id, children }) => {
 
   return (
     <div
-      className="action-bar absolute top-1/2 -left-[1.5rem] -translate-y-1/2 z-10 flex flex-col items-center opacity-0 translate-x-4 
-        group-hover:opacity-100 group-hover:translate-x-0 transition-all duration-300 ease-in-out"
+      className="action-bar absolute -bottom-4 left-1/2 -translate-x-1/2 md:bottom-1/2 md:translate-y-1/2 md:-left-[1.5rem] z-10 flex md:flex-col items-center opacity-0 md:translate-x-4 
+        group-hover:opacity-100 group-hover:translate-y-4 md:group-hover:translate-y-1/2 md:group-hover:translate-x-0 transition-all duration-300 ease-in-out"
     >
       {/* Drag Button */}
       {children}
+
       {/* Delete Button */}
+      {/* See styles/action-bar.scss for styling */}
       <button onClick={() => deleteCell(id)} aria-label="Delete Cell">
-        <i className="fas fa-trash-alt text-2xl" />
+        <i className="fas fa-trash-alt" />
       </button>
     </div>
   );
